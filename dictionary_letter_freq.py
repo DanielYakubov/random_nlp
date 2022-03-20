46332e257c99acc5e31e7757d6c4ea93a59f9a9a
DanielYakubov <Danielyak98@gmail.com>
2022-03-19 23:27:09 -0400

    removing old code

--- dictionary_letter_freq.py
+++ dictionary_letter_freq.py
@@ -21,7 +21,7 @@ def char_counter(word_list: List) -> Counter():
     return c
 
 
-def compare_frequencies(c1: Counter(), c2: Counter()) -> defaultdict:
+def compare_frequencies(c1: Counter(), c2: Counter()) -> defaultdict():
     """returns a dictionary where the key is a common key of both dictionaries
     and the value is a list of frequencies, which is computed by dividing the count
     by the total of the counts in the counter object."""
@@ -30,29 +30,22 @@ def compare_frequencies(c1: Counter(), c2: Counter()) -> defaultdict:
         total_letters = sum(c.values())
         for letter, count in c.items():
             if letter in string.ascii_lowercase:
-                freq = round(count/total_letters, 30) * 100
+                freq = round(count / total_letters, 30) * 100
                 d[letter].append(freq)
     return d
 
 
-def show_bar_graph(data: dict) -> None:
-    """displays values for a dictionary containing
-    k:v pairs where v is a list containing 2 items"""
-    keys, values_list = list(data.items())
-
-    for lst in values_list:
-        pass
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     wrds = words.words()
-    brwn = brown.words() # don't want to remove stop words as we are looking a letter frequencies.
+    brwn = (
+        brown.words()
+    )  # don't want to remove stop words as we are looking a letter frequencies.
     c_words = char_counter(wrds)
     c_brown = char_counter(brwn)
     d = compare_frequencies(c_words, c_brown)
 
     for x, v in d.items():
-        print(x, ':', v)
+        print(x, ":", v)
 
     # plotting
     width = 0.35
@@ -63,13 +56,24 @@ if __name__ == '__main__':
     bar1 = np.arange(len(wrds_values))
     bar2 = [x + width for x in bar1]
 
-    plt.bar(bar1, wrds_values, color='g', width=width,
-            edgecolor='black', label='Words Corpus')
-    plt.bar(bar2, brwn_values, color='b', width=width,
-            edgecolor='black', label='Brown Corpus')
-    plt.xlabel('Letter', fontweight='bold', fontsize=12)
-    plt.ylabel('Frequency as a %', fontweight='bold', fontsize=12)
-    plt.xticks([r + width for r in range(len(wrds_values))],
-               letters)
+    plt.bar(
+        bar1,
+        wrds_values,
+        color="g",
+        width=width,
+        edgecolor="black",
+        label="Words Corpus",
+    )
+    plt.bar(
+        bar2,
+        brwn_values,
+        color="b",
+        width=width,
+        edgecolor="black",
+        label="Brown Corpus",
+    )
+    plt.xlabel("Letter", fontweight="bold", fontsize=12)
+    plt.ylabel("Frequency as a %", fontweight="bold", fontsize=12)
+    plt.xticks([r + width for r in range(len(wrds_values))], letters)
     plt.legend()
     plt.show()
